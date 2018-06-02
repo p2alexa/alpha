29b1b8f2300c1c97af459de90930d059116eb6e8
Ramnath Sharma <axel-ram@users.noreply.github.com>
2018-06-03 01:53:18 +0530

    Update play_me.py

--- play_me.py
+++ play_me.py
@@ -11,6 +11,7 @@ def play_song(text):
 	print('which song would you like to play')
 	try:
 		#text=listener.listen()
+		#you can give input with spaces but only give keywords that are available in one song
 		text=str(input('enter song name'))
 		print('alexa thinks you said',text)
 		text="\ ".join(text.split())
@@ -20,6 +21,7 @@ def play_song(text):
 		#if all(x in subprocess.check_output('ls /home/'+getpass.usrname()+'Music/').decode().split('\n') for x in text):
 		try:
 			result = subprocess.check_output('find /home/'+getpass.getuser()+'/Music/ -iname *'+text+'*',shell=True).decode()
+			#song not found
 			if result == "":
 				print("could not find the song")
 			else:
@@ -32,8 +34,8 @@ def play_song(text):
 			
 		except Exception as e:
 			print("oops... looks like I could not find the song you asked.",e)
-	except:
-		print("alexa could not understand what you said")
+	except Exception as e:
+		print("alexa could not understand what you said",e)
 
 play_song("play a song")
 		
